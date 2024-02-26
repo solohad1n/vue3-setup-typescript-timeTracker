867bc4ac5b937e0c5cab298a1a9092573fdd1558
solohad1n <fox663399@gmail.com>
2024-02-26 12:30:27 +0600

    init

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ICONS } from '../module/icons'
+import { isIconValid } from '../module/validators'
+
+defineProps({
+  name: {
+    required: true,
+    type: String,
+    validator: isIconValid
+  }
+})
+</script>
+
+<template>
+  <component :is="ICONS[name]" :class="$attrs.class || 'h-8'" />
+</template>
---
+++ src/components/TheLogo.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { PAGE_TIMELINE } from '../module/constants'
+import { currentPage, navigate } from '../router'
+import { scrollToCurrentHour } from '../module/timeline-items'
+
+function handleClick() {
+  currentPage.value === PAGE_TIMELINE ? scrollToCurrentHour(true) : navigate(PAGE_TIMELINE)
+}
+</script>
+
+<template>
+  <a :href="`#${PAGE_TIMELINE}`" @click="handleClick">
+    <img src="../assets/img/logo.png" alt="Logo" class="h-9" />
+  </a>
+</template>
