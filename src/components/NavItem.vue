8484faa30c252ddc26949a03a4defdd3b38d18fc
solohad1n <fox663399@gmail.com>
2024-02-26 19:26:54 +0600

    Typing the NavItem component in TS

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,25 +1,21 @@
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue'
 import { PAGE_TIMELINE } from '../module/constants'
-import { isNavItemValid } from '../module/validators'
 import { currentPage, navigate } from '../router'
 import { scrollToCurrentHour } from '../module/timeline-items'
 import BaseIcon from './BaseIcon.vue'
+import type { NavItems } from '../types/types'
 
-const props = defineProps({
-  navItem: {
-    required: true,
-    type: Object,
-    validator: isNavItemValid
-  }
-})
+const props = defineProps<{
+  navItem: NavItems
+}>()
 
-const classes = computed(() => [
+const classes = computed(():string[] => [
   'flex flex-col items-center p-2 text-xs capitalize',
   props.navItem.page === currentPage.value ? 'bg-gray-200' : 'hover:bg-gray-100'
 ])
 
-function handleClick() {
+function handleClick():void {
   currentPage.value === PAGE_TIMELINE && props.navItem.page === PAGE_TIMELINE
     ? scrollToCurrentHour(true)
     : navigate(props.navItem.page)
