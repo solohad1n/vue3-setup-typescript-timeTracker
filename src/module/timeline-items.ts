f34e45184e1d32afecbc7fb1ed6a10e6f4fff51c
solohad1n <fox663399@gmail.com>
2024-02-26 19:53:10 +0600

    Typing the TheTimelineIndicator component in TS

--- src/module/timeline-items.ts
+++ src/module/timeline-items.ts
@@ -1,10 +1,10 @@
-import { computed, ref, watch } from 'vue'
+import { computed, ref, watch, ComponentPublicInstance } from 'vue'
 import { HOURS_IN_DAY, MIDNIGHT_HOUR } from './constants'
 import { endOfHour, isToday, now, toSeconds, today } from './time'
 import { stopTimelineItemTimer } from './timeline-item-timer'
 import type { Activity, State, TimeLineItem } from '../types/types'
 
-export const timelineItemRefs = ref<any>([])
+export const timelineItemRefs = ref<ComponentPublicInstance[] | null>(null)
 
 
 export const timelineItems = ref<TimeLineItem[]>([])
@@ -59,7 +59,7 @@ export function scrollToCurrentHour(isSmooth = false):void {
 }
 
 export function scrollToHour(hour:number, isSmooth = true):void {
-  const el:any = hour === MIDNIGHT_HOUR ? document.body : timelineItemRefs.value[hour - 1].$el
+  const el:HTMLBodyElement | HTMLLIElement = hour === MIDNIGHT_HOUR || !timelineItemRefs.value ? document.body : timelineItemRefs.value[hour - 1].$el
 
   el.scrollIntoView({ behavior: isSmooth ? 'smooth' : 'instant' })
 }
